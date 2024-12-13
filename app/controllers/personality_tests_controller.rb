ad61ad1f14612a3007979986270c4844eafed8eb
Yusuke <yusuke.052835@gmail.com>
2024-12-13 17:40:45 +0900

    診断結果追加

--- app/controllers/personality_tests_controller.rb
+++ app/controllers/personality_tests_controller.rb
@@ -3,4 +3,30 @@ class PersonalityTestsController < ApplicationController
     @questions = Question.order(:id)
     @personality_test = PersonalityTest.new
   end
+
+  def create
+    @personality_test = PersonalityTest.new(personality_test_params)
+    @result = @personality_test.get_result
+
+    if @result
+      session[:result] = @result.id
+      redirect_to personality_test_path(@result.id)
+    else
+      flash[:alert]
+      render :new
+    end
+  end
+
+  def show
+    @result = Monk.find_by(id:session[:result])
+    unless @result
+      redirect_to new_personality_test_path
+    end
+  end
+
+  private
+
+  def personality_test_params
+    params.require(:personality_test).permit(answers: {})
+  end
 end
--- app/models/personality_test.rb
+++ app/models/personality_test.rb
@@ -1,4 +1,22 @@
 class PersonalityTest
   include ActiveModel::Model
   attr_accessor :answers
+
+  def calculate_score
+    score = 0
+    questions = Question.all
+    questions.each do |question|
+      answer = answers[question.id.to_s]
+      if answer
+        score_mapping = question.score_mappings.find_by(option: answer.to_i)
+        score += score_mapping.score if score_mapping
+      end
+    end
+    score
+  end
+
+  def get_result
+    total_score = calculate_score
+    Monk.find_by('start_score <= ? AND end_score >= ?', total_score, total_score)
+  end
 end
