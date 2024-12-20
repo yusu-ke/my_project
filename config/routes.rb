e50fe04c3e5a7d7255a4a2161f135766a4476653
Yusuke <yusuke.052835@gmail.com>
2024-12-20 15:01:25 +0900

    Lint修正

--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "home#index"
   resources :personality_tests, only: [ :new, :create, :show ]
-  resources :posts, only: [:index]
+  resources :posts, only: [ :index ]
 end
