842edc1ee1bf0d532a1127cf4a830e0d8814106d
Yusuke <yusuke.052835@gmail.com>
2024-12-22 22:30:49 +0900

    add:掲示板作成機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,20 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to posts_path, success: "掲示板を作成しました。"
+    else
+      flash.now[:danger] = "掲示板の作成に失敗しました。"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:temple_name, :location, :comment)
+  end
 end
