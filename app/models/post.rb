e99900ae61b292d749ec39140a203bb7e7a72999
Yusuke <yusuke.052835@gmail.com>
2024-12-23 12:06:33 +0900

    add:画像投稿機能実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,7 @@ class Post < ApplicationRecord
   validates :location, presence: true, length: { maximum: 20 }
   validates :comment, presence: true, length: { maximum: 255 }
 
+  mount_uploader :post_image, PostImageUploader
+
   belongs_to :user
 end
