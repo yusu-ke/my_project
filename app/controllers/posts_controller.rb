9a614de0fa2f0c2cddd04440011e48829c8fc769
Yusuke <yusuke.052835@gmail.com>
2024-12-24 16:25:02 +0900

    add:掲示板編集・削除

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,6 +21,25 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def edit
+    @post = current_user.posts.find(params[:id])
+  end
+
+  def update
+    @post = current_user.posts.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path(@post), success: "掲示板を更新しました。"
+    else
+      flash.now[:danger] = "更新に失敗しました。"
+      render :edit, status: :unprocessable_entity
+    end
+  end
+  
+  def destroy
+    post = current_user.posts.find(params[:id])
+    post.destroy!
+    redirect_to posts_path, success: "削除しました。"
+  end
 
   private
 
